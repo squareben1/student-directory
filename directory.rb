9feedc2b4f0fd5598dca4f8838cc8bb1887e1947
Squareben1 <bengittins@live.com>
2019-12-03 15:52:09 +0000

    Added Method to return names beginning with certain letter

--- directory.rb
+++ directory.rb
@@ -1,17 +1,17 @@
 
 # #put all students into array 
 # students = [ 
-#   # {name: "Dr. Hannibal Lecter", cohort: :november},
-#   # {name: "Darth Vader", cohort: :november},
-#   # {name: "Nurse Ratched", cohort: :november},
-#   # {name: "Michael Corleone", cohort: :november},
-#   # {name: "Alex DeLarge", cohort: :november},
-#   # {name: "The Wicked Witch of the West", cohort: :november},
-#   # {name: "Terminator", cohort: :november},
-#   # {name: "Freddy Krueger", cohort: :november},
-#   # {name: "The Joker", cohort: :november},
-#   # {name: "Joffrey Baratheon", cohort: :november},
-#   # {name: "Norman Bates", cohort: :november}
+#   {name: "Dr. Hannibal Lecter", cohort: :november},
+#   {name: "Darth Vader", cohort: :november},
+#   {name: "Nurse Ratched", cohort: :november},
+#   {name: "Michael Corleone", cohort: :november},
+#   {name: "Alex DeLarge", cohort: :november},
+#   {name: "The Wicked Witch of the West", cohort: :november},
+#   {name: "Terminator", cohort: :november},
+#   {name: "Freddy Krueger", cohort: :november},
+#   {name: "The Joker", cohort: :november},
+#   {name: "Joffrey Baratheon", cohort: :november},
+#   {name: "Norman Bates", cohort: :november}
 # ]
 
 def input_students
@@ -44,11 +44,20 @@ def print(students)
   end 
 end 
 
+# Method to return names beginning with certain letter
+def print_if_first_letter(students, letter)
+  students.each do |student, index|
+  if student[:name][0] == letter
+    puts "Students with names beginning with #{letter}: #{student[:name]} (#{student[:cohort]} cohort)" 
+  end 
+  end 
+end 
+
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end 
+
 # nothing happens until we call the methods
 students = input_students
 print_header
-print(students)
 print_footer(students)
