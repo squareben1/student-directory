62dbbeecbef482d107e6126c7b7e225dbf2e5786
Squareben1 <bengittins@live.com>
2019-12-05 16:07:13 +0000

    Added load method

--- directory.rb
+++ directory.rb
@@ -125,6 +125,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
 
   puts "9. Exit"
 end 
@@ -145,6 +146,15 @@ def save_students
   file.close
 end 
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line| 
+    name, cohort, hobby = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym, hobby: hobby}
+  end 
+  file.close
+end 
+
 def process(selection)
   case selection
   when "1"
@@ -153,6 +163,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else 
